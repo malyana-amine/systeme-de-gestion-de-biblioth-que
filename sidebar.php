9ae36bbfe0d1f32fce2a466148ce8ba22673598b
amine-malyana <aminemalyana12@gmail.com>
2022-11-22 17:03:50 +0100

    add sidbar 100%

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,20 @@
 <div class="bg-dark sticky-top col-2 d-flex flex-column justify-content-between vh-100">
 
-<img class="logo2 px-5 py-2 " src="maquette/logo/Untitled-2.png" alt="">
+<img class="logo2 p-2 " src="maquette/logo/Untitled-2.png" alt="">
 
 <div class="text-start d-flex  pb-5 mb-5 ">
-    <div class="col-4"></div>
+    <div class="col-3"></div>
     <ul class="text-white list-unstyled ">
-        <li class="py-3 d-flex"><i class=" pe-3 pt-1 fa-solid fa-house-user"></i><a class="text-decoration-none text-white d-none d-md-block " href="#"> dashboard</a> </li>
-        <li class="py-3 d-flex"><i class="pe-3 pt-1 fa-solid fa-book"></i><a class="text-decoration-none text-white d-none d-md-block" href="#"> Books</a> </li>
-        <li class="py-3 d-flex"><i class="pe-3 pt-1 fa-solid fa-book "></i><a class="text-decoration-none text-white d-none d-md-block" href="#"> users</a> </li>
+        <li class="py-3 d-flex"><i title="dashboard" class=" pe-3 pt-1 fa-solid fa-house-user"></i><a class="text-decoration-none text-white d-none d-md-block " title="dashboard" href="#"> dashboard</a> </li>
+        <li class="py-3 d-flex"><i title="Books" class="pe-3 pt-1 fa-solid fa-book"></i><a class="text-decoration-none text-white d-none d-md-block" title="Books" href="#"> Books</a> </li>
+        <li class="py-3 d-flex"><i title="users" class="pe-3 pt-1 fa-solid fa-users "></i><a class="text-decoration-none text-white d-none d-md-block" title="users" href="#"> users</a> </li>
     </ul>
 </div>
 <div class=" d-flex flex-column align-items-center">
    
     
         
-<form method="POST" class="btn-group dropup pb-3 ">
+<form method="POST" class="btn-group dropup pb-3 pe-3 ">
           <div class="align-items-center d-flex ">
           <div id="profilpic" class = "bg-warning rounded-circle fs-5 divpad " style="width:35px ; height:35px" ></div>
             <h6 class="text-white ps-1 pe-3 pt-2 d-none d-sm-block" id="flname"><?php echo  $_SESSION['first_name'].' '.$_SESSION['last_name'] ?></h6>
